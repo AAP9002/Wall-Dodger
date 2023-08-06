ca7334728df8501e96b4b8cd371f5427ad68e467
aap9002 <aap9002@gmail.com>
2023-08-06 12:54:39 +0100

    Moving walls and score ++

--- client/src/Game.js
+++ client/src/Game.js
@@ -1,8 +1,13 @@
 import './game.css';
 import * as THREE from 'three';
-import { useEffect } from 'react';
+import { useEffect,useState} from 'react';
 
 function Game() {
+    // counter for score
+    const [score, setScore] = useState(0);
+
+
+    // create renderer
     const renderer = new THREE.WebGLRenderer();
 
     useEffect(() => {
@@ -16,6 +21,7 @@ function Game() {
         }
         window.addEventListener('resize',handleResize);
 
+        // event listener to move bob
         var xSpeed = 0.1;
         var ySpeed = 0.1;
 
@@ -42,42 +48,79 @@ function Game() {
         // eslint-disable-next-line
     }, []);
 
+    // create scene
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild(renderer.domElement);
 
+    // create bob
     const bobGeometry = new THREE.BoxGeometry();
-    const bobMaterial = new THREE.MeshNormalMaterial({color:0x00ff00});
-
+    const bobMaterial = new THREE.MeshNormalMaterial({color:0xff0000});
     const bob = new THREE.Mesh(bobGeometry,bobMaterial);
     scene.add(bob);
-
-    const groundGeometry = new THREE.BoxGeometry(5,0.01,20);
-    const groundMaterial = new THREE.MeshNormalMaterial({color:0x0000ff});
-
-    const ground = new THREE.Mesh(groundGeometry,groundMaterial);
-    ground.position.z = -5;
-    scene.add(ground);
-
-    const wallGeometry = new THREE.BoxGeometry(0,5,4);
-    const wallMaterial = new THREE.MeshNormalMaterial({color:0x0000ff});
-    scene.add(new THREE.Mesh(wallGeometry,wallMaterial));
-
-    camera.position.z = 7;
-    camera.position.y = 3;
-    camera.position.x = 3;
-
-    camera.rotation.y = 0.2;
-
+    bob.position.z = 3.5;
     bob.position.y = 1;
 
+    // create ground
+    const groundGeometry = new THREE.BoxGeometry(5,0.01,40);
+    const groundMaterial = new THREE.MeshLambertMaterial({color:0xff00ff});
+    const ground = new THREE.Mesh(groundGeometry,groundMaterial);
+    ground.position.z = -2;
+    scene.add(ground);
+
+    // create wall
+
+    // Generate Wall Function
+    const generateWall = (zPos) => {
+        const wallGeometry = new THREE.BoxGeometry(5,4,0.4);
+        const wallMaterial = new THREE.MeshNormalMaterial({color:0x0000ff});
+        const wall = new THREE.Mesh(wallGeometry,wallMaterial);
+        wall.position.z = zPos;
+        wall.position.y = 2;
+        scene.add(wall);
+        wallList.push(wall);
+    }
+
+    const wallList = [];
+    for (var i = 0; i < 10; i++) {
+        generateWall((-20*i)-10);
+    }
+
+    // camera position
+    camera.position.z = 8;
+    camera.position.y = 4;
+    camera.position.x = 3;
+    camera.rotation.y = 0.2;
+
+    //create light
+    const light = new THREE.AmbientLight(0xffffff,1,1000);
+    light.position.set(5,6,5);
+    scene.add(light);
+
+    // animation loop
     const animate = function(){
         requestAnimationFrame(animate);
 
-        bob.rotation.x += 0.01;
-        bob.rotation.y += 0.01;
+        // bob.rotation.x += 0.01;
+        // bob.rotation.y += 0.01;
+
+        if (wallList.length > 0) {
+            if (wallList[0].position.z>(bob.position.z + 0.5)){
+                console.log("Wall Passed");
+                setScore(score+1);
+                //remove the first wall
+                scene.remove(wallList[0]);
+                // remove the first wall from the array and add a new wall
+                wallList.shift();
+                generateWall(-210);
+            }
+        }
+
+        for (var i = 0; i < wallList.length; i++) {
+            wallList[i].position.z += 0.01;
+        }
 
         renderer.render(scene,camera);
     }
@@ -86,6 +129,7 @@ function Game() {
 
     return (
       <>
+        <p className='overlayText'>Score: {score}</p>
       </>
     );
   }
